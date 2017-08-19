9c19ead559be963c782ec4e92a84748bcff6e8aa
Patrick McCarren <patrick@mmsc.io>
2017-08-19 17:49:57 -0400

    fix: add missing EventServiceProvider

---
+++ infrastructure/Events/EventServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Infrastructure\Events;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as IlluninateEventServiceProvider;
+
+class EventServiceProvider extends IlluninateEventServiceProvider
+{
+
+}
