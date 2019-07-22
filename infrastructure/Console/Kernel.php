c7f176f9aa2efe6a6f5200a80baf285b820ecade
Esben Petersen <esbenspetersen@gmail.com>
2019-07-22 11:27:16 +0200

    add script to automatically register commands

--- infrastructure/Console/Kernel.php
+++ infrastructure/Console/Kernel.php
@@ -2,7 +2,12 @@
 
 namespace Infrastructure\Console;
 
-use Api\Users\Console\AddUserCommand;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use Illuminate\Console\Application as Artisan;
+use Illuminate\Console\Command;
+use Symfony\Component\Finder\Finder;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +19,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        AddUserCommand::class
+        
     ];
 
     /**
@@ -27,5 +32,45 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+    }
+
+    protected function commands()
+    {
+        $config = config('optimus.components');
+
+        foreach ($config['namespaces'] as $namespace => $path) {
+            $subDirectories = glob(sprintf('%s%s*', $path, DIRECTORY_SEPARATOR), GLOB_ONLYDIR);
+
+            $paths = [];
+
+            foreach ($subDirectories as $componentRoot) {
+                $component = substr($componentRoot, strrpos($componentRoot, DIRECTORY_SEPARATOR) + 1);
+
+                $commandDirectory = sprintf('%s%sConsole', $componentRoot, DIRECTORY_SEPARATOR);
+
+                if (file_exists($commandDirectory)) {
+                    $paths[] =  $commandDirectory;
+                }
+            }
+
+            if (empty($paths)) {
+                continue;
+            }
+
+            foreach ((new Finder)->in($paths)->files() as $command) {
+                $command = $namespace.str_replace(
+                    ['/', '.php'],
+                    ['\\', ''],
+                    Str::after($command->getPathname(), $path)
+                );
+
+                if (is_subclass_of($command, Command::class) &&
+                    ! (new ReflectionClass($command))->isAbstract()) {
+                    Artisan::starting(function ($artisan) use ($command) {
+                        $artisan->resolve($command);
+                    });
+                }
+            }
+        }
     }
 }
